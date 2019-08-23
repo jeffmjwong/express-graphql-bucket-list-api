46acdadf729ae6ac405308889f1cdcd2065bccac
Jeff Wong <jeffmjwong@gmail.com>
2019-08-23 20:23:39 +1000

    Add a GET route for getting node_env in index.js

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const port = process.env.PORT || 3001;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Express and GraphQL server!!!');
+  res.send('Welcome to Express and GraphQL server!');
+});
+
+app.get('/node-env', (req, res) => {
+  res.send(process.env.NODE_ENV);
 });
 
 app.listen(port, () => {
