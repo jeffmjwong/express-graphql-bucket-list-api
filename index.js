57ff541999f16b832897ba511196adbfed78ba9b
Jeff Wong <jeffmjwong@gmail.com>
2019-08-23 19:52:26 +1000

    Add a GET route for '/' in index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from 'express';
 const port = process.env.PORT || 3001;
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Express and GraphQL server!!!');
+});
+
 app.listen(port, () => {
   console.log(`Express server is now listening on port ${port}...`)
 });
