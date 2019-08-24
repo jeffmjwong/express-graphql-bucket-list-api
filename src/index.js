5a61e8873c0ad1e96f65f920923c49fe9438ac61
Jeff Wong <jeffmjwong@gmail.com>
2019-08-24 20:43:44 +1000

    Create a route to select rows from db in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import db from './db';
+
 const port = process.env.PORT || 3001;
 const app = express();
 
@@ -9,6 +11,17 @@ app.get('/', (req, res) => {
 
 app.get('/node-env', (req, res) => {
   res.send(process.env.NODE_ENV);
+});
+
+app.get('/lists', (req, res) => {
+  db.any('SELECT * FROM bucket_items')
+    .then(data => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch(error => {
+      console.log(err);
+    });
 });
 
 app.listen(port, () => {
