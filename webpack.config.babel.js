b99fa92074a8b3fd2c506962095bb5506087f559
Jeff Wong <jeffmjwong@gmail.com>
2019-08-24 20:10:51 +1000

    Update entry point in webpack config file

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,7 +4,7 @@ import terserWebpackPlugin from 'terser-webpack-plugin';
 export default {
   mode: 'production',
   target: 'node',
-  entry: path.resolve(__dirname, 'index.js'),
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
