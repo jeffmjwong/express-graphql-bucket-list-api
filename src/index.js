06ded91baab58f584437bb57308464c17b42e85c
Jeff Wong <jeffmjwong@gmail.com>
2019-08-25 16:11:48 +1000

    Instantiate apollo graphQL server in src/index.js file

--- src/index.js
+++ src/index.js
@@ -1,29 +1,51 @@
 import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
 import db from './db';
 
 const port = process.env.PORT || 3001;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Express and GraphQL server!');
-});
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-app.get('/node-env', (req, res) => {
-  res.send(process.env.NODE_ENV);
-});
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world from Apollo Server Express!',
+  },
+};
 
-app.get('/lists', (req, res) => {
-  db.any('SELECT * FROM bucket_items')
-    .then(data => {
-      console.log(data);
-      res.json(data);
-    })
-    .catch(error => {
-      console.log(err);
-    });
-});
+const graphqlServer = new ApolloServer({ typeDefs, resolvers });
+console.log(graphqlServer.graphqlPath);
+
+graphqlServer.applyMiddleware({ app });
 
 app.listen(port, () => {
-  console.log(`Express server is now listening on port ${port}...`)
+  console.log(`Express server is now ready at http://localhost:${port}${graphqlServer.graphqlPath}`);
 });
+
+// app.get('/', (req, res) => {
+//   res.send('Welcome to Express and GraphQL server!');
+// });
+
+// app.get('/node-env', (req, res) => {
+//   res.send(process.env.NODE_ENV);
+// });
+
+// app.get('/lists', (req, res) => {
+//   db.any('SELECT * FROM bucket_items')
+//     .then(data => {
+//       console.log(data);
+//       res.json(data);
+//     })
+//     .catch(error => {
+//       console.log(err);
+//     });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Express server is now listening on port ${port}...`)
+// });
